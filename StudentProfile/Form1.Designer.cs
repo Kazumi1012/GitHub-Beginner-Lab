4d51a1b08b9c05cb2e8cd5965301195d66b73160
Shairo Andrew Creus <shairoandrew19@gmail.com>
2026-09-13 18:29:30 +0800

    please verified my changes

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -61,7 +61,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(392, 31);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Contact Number: 09955274821";
+            this.label2.Text = "Contact Number: 09955274822";
             // 
             // Form1
             // 
