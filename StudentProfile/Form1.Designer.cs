c9d60e132726ec4d8b30a34a5f1490cff63010c1
Shairo Andrew Creus <shairoandrew19@gmail.com>
2026-09-13 22:34:43 +0800

    I added some email

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -67,9 +67,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(128, 326);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 324);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(171, 13);
+            this.label3.Size = new System.Drawing.Size(379, 25);
             this.label3.TabIndex = 3;
             this.label3.Text = "Email: shairoandrew19@gmail.com";
             // 
